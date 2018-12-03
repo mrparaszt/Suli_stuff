6616892151f78d7eac5e13c75ac70b53af2c2f44
mrparaszt <45559971+mrparaszt@users.noreply.github.com>
2018-12-03 15:35:06 +0200

    Add files via upload

---
+++ fibo/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int fibo(int n);
+
+int main()
+{
+    int n;
+    cout<<"n=";
+    cin>>n;
+    cout<<fibo(n);
+    return 0;
+}
+
+int fibo(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    else
+        return fibo(n-1)+fibo(n-2);
+}
