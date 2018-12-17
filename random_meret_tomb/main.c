e378e1366af9f20eb8a5b89fbe67b3444261f697
mrparaszt <45559971+mrparaszt@users.noreply.github.com>
2018-12-17 18:43:29 +0200

    Add files via upload

--- random_meret_tomb/main.c
+++ random_meret_tomb/main.c
@@ -10,15 +10,14 @@ int main()
     f = fopen("input.txt", "rt");
     fscanf(f, "%d", &n);
     l = l << n;
-
-    printf("%d\n", l);
     teszt = teszt | l;
-    printf("%d\n", teszt);
+
+
     int nr = 0;
     while (teszt % 2 == 0)
     {
-        teszt >> 2;
-        printf("%d", teszt);
+        teszt /= 2;
+        //printf("%d", teszt);
         nr++;
     }
     printf("%d", nr);
